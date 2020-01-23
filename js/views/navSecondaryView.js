63e360092698e881265868622b82f614a0ac88fd
Tom <metaphorps@gmail.com>
2020-01-22 21:23:33 -0600

    refactored nav2

--- js/views/navSecondaryView.js
+++ js/views/navSecondaryView.js
@@ -9,67 +9,28 @@ import {
 export const updateNavSecondaryTitles = (navPrimaryId) => {
     // console.log(navPrimaryId);
     if (navPrimaryId === 'tab-0') {
-        // get array of nav secondary tabs
-        let navSecondaryTitlesArr = Array.from(document.querySelectorAll(".nav-secondary-tab-0"));
-
-        // titles and counter to used when we loop through the nav2 elements
-        const titles = navSecondaryTitles.audio; // saves array of audio nav titles
-        let titlesCount = 0; // counter for number of titles
-
-        navSecondaryTitlesArr.forEach(el => {
-            el.textContent = `${titles[titlesCount]}`;
-            titlesCount = titlesCount + 1;
-        });
+        const navSecondaryTitlesArr = Array.from(document.querySelectorAll(".nav-secondary-tab-0")); // creates array of current tabs to target for title replacement
+        const titles = navSecondaryTitles.audio; // saves array of new nav titles
+        replaceNavSecondaryTitles(navSecondaryTitlesArr, titles); // replaces old titles with new titles
     } else if (navPrimaryId === 'tab-1') {
-        // get array of nav secondary tabs
-        const navSecondaryTitlesArr = Array.from(document.querySelectorAll(".nav-secondary-tab-1"));
-
-        // titles and counter to used when we loop through the nav2 elements
-        const titles = navSecondaryTitles.video; // saves array of audio nav titles
-        let titlesCount = 0; // counter for number of titles
-
-        //  nav secondary titles
-        navSecondaryTitlesArr.forEach(el => {
-
-            el.textContent = `${titles[titlesCount]}`;
-            titlesCount = titlesCount + 1;
-
-        });
+        const navSecondaryTitlesArr = Array.from(document.querySelectorAll(".nav-secondary-tab-1")); // creates array of current tabs to target for title replacement
+        const titles = navSecondaryTitles.video; // saves array of new nav titles
+        replaceNavSecondaryTitles(navSecondaryTitlesArr, titles); // replaces old titles with new titles
     } else if (navPrimaryId === 'tab-2') {
-        // get array of nav secondary tabs
-        const navSecondaryTitlesArr = Array.from(document.querySelectorAll(".nav-secondary-tab-2"));
-
-        // titles and counter to used when we loop through the nav2 elements
-        const titles = navSecondaryTitles.audioTools; // saves array of audio nav titles
-        let titlesCount = 0; // counter for number of titles
-
-        //  nav secondary titles
-        navSecondaryTitlesArr.forEach(el => {
-
-            el.textContent = `${titles[titlesCount]}`;
-            titlesCount = titlesCount + 1;
-        });
+        const navSecondaryTitlesArr = Array.from(document.querySelectorAll(".nav-secondary-tab-2")); // creates array of current tabs to target for title replacement
+        const titles = navSecondaryTitles.audioTools; // saves array of new nav titles
+        replaceNavSecondaryTitles(navSecondaryTitlesArr, titles); // replaces old titles with new titles
     } else if (navPrimaryId === 'tab-3') {
-        // get array of nav secondary tabs
-        const navSecondaryTitlesArr = Array.from(document.querySelectorAll(".nav-secondary-tab-3"));
-
-        // titles and counter to used when we loop through the nav2 elements
-        const titles = navSecondaryTitles.placeholder; // saves array of audio nav titles
-        let titlesCount = 0; // counter for number of titles
-
-        //  nav secondary titles
-        navSecondaryTitlesArr.forEach(el => {
-
-            el.textContent = `${titles[titlesCount]}`;
-            titlesCount = titlesCount + 1;
-        });
+        const navSecondaryTitlesArr = Array.from(document.querySelectorAll(".nav-secondary-tab-3")); // creates array of current tabs to target for title replacement
+        const titles = navSecondaryTitles.placeholder; // saves array of new nav titles
+        replaceNavSecondaryTitles(navSecondaryTitlesArr, titles); // replaces old titles with new titles
     }
 }
 
 const replaceNavSecondaryTitles = (targetTabs, newTitles) => {
     let titlesCount = 0; // counter for number of titles
 
-
+    // replaces old titles with new titles
     targetTabs.forEach(el => {
         el.textContent = `${newTitles[titlesCount]}`;
         titlesCount = titlesCount + 1;
