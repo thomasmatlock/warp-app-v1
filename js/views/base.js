52364529445eda957ab9e5396aad9f235f6d2dff
Tom <metaphorps@gmail.com>
2020-01-27 21:06:48 -0600

    implementing clean hierarchy all around

--- js/views/base.js
+++ js/views/base.js
@@ -1,6 +1,6 @@
 export const elements = {
     nav_A: document.querySelector('.nav_A'),
-    navPrimaryTab: document.querySelector('.nav_A-tab'),
+    nav_A_tab: document.querySelector('.nav_A-tab'),
     navSecondaryTabs: document.querySelector('.nav_B'),
     navSecondaryTab: document.querySelector('.nav_B-tab'),
     content__list: document.querySelector('.content__list')
