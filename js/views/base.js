288e38d95583dd4f4e99eb6221c6bd64ba76e5fb
Tom <metaphorps@gmail.com>
2020-02-04 21:04:50 -0600

    ...

--- js/views/base.js
+++ js/views/base.js
@@ -2,10 +2,10 @@ export const elements = {
     nav_A: document.querySelector('.nav_A'),
     nav_A_tab: document.querySelector('.nav_A-tab'),
     nav_B: document.querySelector('.nav_B'),
-    nav_B_0: document.querySelector('.nav_B_0'),
-    nav_B_1: document.querySelector('.nav_B_1'),
-    nav_B_2: document.querySelector('.nav_B_2'),
-    nav_B_3: document.querySelector('.nav_B_3'),
+    nav_B0: document.querySelector('.nav_B0'),
+    nav_B1: document.querySelector('.nav_B1'),
+    nav_B2: document.querySelector('.nav_B2'),
+    nav_B3: document.querySelector('.nav_B3'),
     nav_B_button: document.querySelector('.nav_B-button'),
     content__list: document.querySelector('.content__list')
 }
