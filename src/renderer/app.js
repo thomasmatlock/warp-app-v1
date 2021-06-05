d0d995291901f731c0441ec3e41fc1240010734e
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2021-06-05 13:00:17 -0500

    LATEST

--- src/renderer/app.js
+++ src/renderer/app.js
@@ -66,7 +66,7 @@ let storage;
         for (var key in storage.user.prefs) {
             // if key value is true, and contains startupTab
             // NEED THIS VERSION
-
+            // LATEST
             if (storage.user.prefs[key] && key.toLowerCase().includes('startuptab')) {
                 let startupTab = key.toLowerCase().slice(19, key.length)
                 return startupTab;
