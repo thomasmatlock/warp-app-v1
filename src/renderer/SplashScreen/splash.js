f11738a7be7b46f793282da8104f43f8c532f80c
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-05-20 20:08:43 -0400

    these status sentences are going well

--- src/renderer/SplashScreen/splash.js
+++ src/renderer/SplashScreen/splash.js
@@ -4,28 +4,61 @@ const directObjArr = require('./nouns.js');
 const adjectiveDescriptiveArr = require('./adjectiveDescriptiveArr.js');
 const qualifier = require('./qualifier.js');
 const adverbs = require('./adverbs.js');
+// const theme = require('./theme.js');
 const package = require('../../../package.json');
 const element = document.getElementById('status1');
-const delay = 2000;
+const delay = 1000;
+let status1, status2, status3;
 window.addEventListener(
     'load',
     function() {
-        element.innerHTML = sentenceGenerator();
 
-        // document.getElementById('version').innerHTML = `${package.version}`; // display app version
-        // let element = document.createElement('h1');
-        // element.innerHTML = 'hello';
-        // element.style.color = 'purple';
-        // const newContent = document.createTextNode("Hi there and greetings!");
-        // document.getElementById('statusContainer').appendChild(newContent);
+        status1 = document.createElement("h1");
+        status1.innerHTML = sentenceGenerator();
+        status1.style.color = 'purple';
+        status1.classList.add('status');
+        status1.classList.add('status1');
+        // status1.classList.add('status1animation');
+        document.getElementById('statusContainer').appendChild(status1);
+        setTimeout(() => {
+            status1.classList.add('becomeStatus2');
+            setTimeout(() => {
+                status1.style.top = '260px';
+                status1.classList.remove('becomeStatus2');
+                status1.classList.add('becomeStatus3');
+                setTimeout(() => {
+                    status1.style.top = '240px';
+                    status1.classList.remove('becomeStatus3');
+                    status1.classList.add('fadeOut');
+                    setTimeout(() => {
+
+                        // status1.classList.remove('becomeStatus2');
+                        // status1.style.top = '240px';
+                    }, 1000);
+                }, 1000);
+            }, 1000)
+        }, 2000)
+
     },
     false
 );
-
+let statusCounter = 0;
 window.setInterval(function() {
-    element.innerHTML = sentenceGenerator();
-    document.getElementById('status2').classList.add('status2');
-    document.getElementById('status3').classList.add('status3');
+    // element.innerHTML = sentenceGenerator();
+    // document.getElementById('status2').classList.add('status2');
+    // document.getElementById('status3').classList.add('status3');
+    // const status1 = document.createElement("h1");
+    // status1.innerHTML = sentenceGenerator();
+    // status1.style.color = 'purple';
+    // status1.classList.add('status');
+    // status1.classList.add('status1');
+    // document.getElementById('statusContainer').appendChild(status1);
+    // status1.classList.add('becomeStatus2');
+    // setTimeout(() => {
+
+    //     status1.classList.add('becomeStatus2');
+    // }, 1000)
+
 }, delay);
 const getRandomInt = (max) => {
     return Math.floor(Math.random() * max);
@@ -33,6 +66,7 @@ const getRandomInt = (max) => {
 
 const easterEggSentences = [
     'hello world',
+    'hmmmm',
     'up up down down left right left right B A',
     `hello ${global.randomFromArray(adjectiveDescriptiveArr)} ${global.randomFromArray(directObjArr)}`,
     `hello ${global.randomFromArray(adjectiveDescriptiveArr)} ${global.randomFromArray(directObjArr)}`,
