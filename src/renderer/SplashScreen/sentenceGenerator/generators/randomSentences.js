1f9cae03c1ff4b109e4b0de839ae6f0ff7367687
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-05-22 22:58:19 -0400

    finished splash screen

--- src/renderer/SplashScreen/sentenceGenerator/generators/randomSentences.js
+++ src/renderer/SplashScreen/sentenceGenerator/generators/randomSentences.js
@@ -1,16 +1,25 @@
 const nouns = require('../components/nouns.js');
 const adjectives = require('../components/adjectives.js');
 const adverbs = require('../components/adverbs.js');
+const phrases = require('../components/phrases.js');
 const verbs = require('../components/verbs.js');
 const global = require('../../../../js/global');
-
+const getRandomPunctuation = () => {
+    let punctuation = ['..', '?', '...', '', '..?'];
+    return global.randomFromArray(punctuation);
+}
 
 const generate = () => {
+    let odds = global.getRandomInt(5);
     let randomAdj = global.randomFromArray(adjectives);
     let randomAdverb = global.randomFromArray(adverbs);
     let randomNoun = global.randomFromArray(nouns);
     let randomVerb = global.randomFromArray(verbs);
+    let randomPunctuation = getRandomPunctuation();
     let sentence = `${randomVerb} ${randomAdverb} ${randomAdj}  ${randomNoun}`;
+    if (odds === 0) {
+        sentence = `${randomVerb} ${randomAdverb} ${randomAdj}  ${randomNoun} (${global.randomFromArray(phrases).toLowerCase()}${randomPunctuation})`;
+    }
     return sentence;
 }
 module.exports = { generate };
