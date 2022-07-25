82d1c82dfe0503b61cbe4ad07910b41be32843d8
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-07-25 16:39:00 -0400

    yes

--- src/renderer/Search/Search.tsx
+++ src/renderer/Search/Search.tsx
@@ -1,10 +1,12 @@
 import { Fragment } from 'react';
 import  './Search.scss';
 // import SearchIcon from '../../assets/public/sections/wrapup/cards/lightning.svg';
+import SearchIcon from '../../../assets/Search/lightning.svg';
+
 const Search = () => {
     return <Fragment>
         <div className="search">
-            {/* <img className="search__input__icon" src={SearchIcon} alt="" /> */}
+            <img className="search__input__icon" src={SearchIcon} />
             <input className="search__input" type="text" />
             {/* <input class="overview_display__search__input" type="text" placeholder="input search. Beep boop">
             {/* <img class="overview_display__search__input__icon " src="img/public/sections/wrapup/cards/lightning.svg" height="15px" alt="" loading="lazy"> */}
