2bd8990d84f92267d3761bda77aba6a323decb2a
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-08-06 18:36:31 -0400

    nice

--- src/renderer/Content/DownloadsAudio.tsx
+++ src/renderer/Content/DownloadsAudio.tsx
@@ -20,25 +20,9 @@ const DownloadsAudio = () => {
     // console.log('mouse enter');
     window.electron.ipcRenderer.sendMessage('browserNotHovered');
   };
-  //  const audioItems = (
-  //    <ul className={classes['cart-items']}>
-  //      {cartCtx.items.map((item) => (
-  //        <CartItem
-  //          key={item.id}
-  //          name={item.name}
-  //          amount={item.amount}
-  //          price={item.price}
-  //          onRemove={cartItemRemoveHandler.bind(null, item.id)}
-  //          onAdd={cartItemAddHandler.bind(null, item)}
-  //        />
-  //      ))}
-  //    </ul>
-  //  );
   return (
     <Fragment>
-      {/* {audioItems} */}
       <div
-        // className="contentPanel"
         className={
           actionBarCtx.isAudioPanelCollapsed
             ? 'contentPanel__collapsed'
