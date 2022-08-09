8b8d047457890ca5a42fd9eb37acc52b2d92a970
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-08-09 16:22:59 -0400

    about to do window title

---
+++ src/renderer/Content/ContextMenu.tsx
@@ -0,0 +1,7 @@
+import { Fragment } from 'react';
+import './ContextMenu.scss';
+
+const ContextMenu = () => {
+  return <Fragment></Fragment>;
+};
+export default ContextMenu;
--- src/renderer/Content/DownloadsVideo.tsx
+++ src/renderer/Content/DownloadsVideo.tsx
@@ -1,16 +1,10 @@
 import { Fragment, useContext } from 'react';
-import iconLength from '../../../assets/Downloads/duration.svg';
-import iconFileSize from '../../../assets/Downloads/fileSize.svg';
-import iconFileResolution from '../../../assets/Downloads/resolution.svg';
-import IconFileTypeVideo from '../../../assets/Downloads/fileTypeVideo.svg';
-import IconFileFps from '../../../assets/Downloads/fps1.svg';
-import thumbnail from '../../../assets/Content/dummythumbnail2.jpg';
-
-import DownloadItem from './DownloadItem';
 
 import ThemeContext from '../../storage/themeContext';
 import ActionBarContext from '../../storage/actionBarContext';
 import DownloadsContext from '../../storage/downloadsContext';
+
+import DownloadItem from './DownloadItem';
 import Sort from './Sort';
 
 import './Downloads.scss';
