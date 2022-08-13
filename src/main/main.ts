6f31e0bba523f6b60a96936ca9059121d11577cb
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-08-13 18:03:16 -0400

    working on it

--- src/main/main.ts
+++ src/main/main.ts
@@ -40,7 +40,7 @@ const getPrefs = () => {
 const setPrefs = (arg: any) => {
   settings.set('prefs', arg);
 };
-// settings.delete('prefs'); // testing only, REMOVE for production
+settings.delete('prefs'); // testing only, REMOVE for production
 let prefs = getPrefs();
 // console.log(prefs);
 //////////////////////////////////////////////////////
@@ -318,7 +318,7 @@ const windowController = {
       } else {
         mWin.show();
         if (view) mWin.maximize();
-        // mWin.maximize();
+        mWin.maximize();
         mWin.webContents.send('appVersion', app.getVersion());
         mWin.webContents.send('main: prefs', prefs);
       }
--- src/storage/prefsContext.tsx
+++ src/storage/prefsContext.tsx
@@ -9,28 +9,67 @@ export const PrefsContextProvider = (props) => {
     prefs = arg;
   });
   const checkboxHandler = (id) => {
-    // console.log(id);
     let newPrefs = { ...prefs };
     newPrefs.general.checkboxes.forEach((checkbox) => {
-      // console.log(checkbox.id, checkbox.checked);
-
       if (checkbox.id === id) {
         checkbox.checked = !checkbox.checked;
       }
     });
-    // console.log(newPrefs.general.checkboxes);
     window.electron.ipcRenderer.sendMessage('main: prefs', newPrefs);
+  };
+  const disableDropdownOptions = (arr) => {
+    arr.forEach((list) => {
+      console.log(list);
+      list.options.forEach((option) => {
+        // option.disabled = true;
+      });
+    });
+  };
+  const getDropdownSelectedOption = () => {};
+  const dropdownHandler = (id, value) => {
+    // console.log(id);
+
+    let newPrefs = { ...prefs };
+    if (id.includes('audio')) {
+      // console.log(newPrefs.audio.dropdowns);
+      if (id.includes('quality')) {
+        console.log(newPrefs.audio.dropdowns);
+      }
+      if (id.includes('format')) {
+        console.log(newPrefs.audio.dropdowns);
+      }
+    }
+    if (id.includes('video')) {
+      console.log('video dropdown');
+    }
+    if (id.includes('warpstagram')) {
+      console.log('warpstagram dropdown');
+    }
+    if (id.includes('general')) {
+      console.log('general dropdown');
+    }
+
+    // console.log(newPrefs);
+
+    newPrefs.general.dropdowns.forEach((dropdown) => {
+      console.log();
+
+      if (dropdown.id === id) {
+        dropdown.value = value;
+      }
+    });
   };
   const parseID = (id) => {
     // console.log(id);
     if (id.includes('dropdown')) {
-      console.log('dropdown', id);
+      // console.log('dropdown', id);
+      dropdownHandler(id);
     }
     if (id.includes('checkbox')) {
       checkboxHandler(id);
     }
     if (id.includes('outputFolder')) {
-      console.log('outputFolder', id);
+      // console.log('outputFolder', id);
     }
   };
   const getID = (id: string) => {
