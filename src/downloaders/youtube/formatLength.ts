8521ea2bbe62e203b46b0a2bd4c36b892b97e52a
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-08-17 12:24:17 -0400

    Update formatLength.ts

--- src/downloaders/youtube/formatLength.ts
+++ src/downloaders/youtube/formatLength.ts
@@ -11,8 +11,22 @@ export default function formatLength(approxSeconds) {
   // var hDisplay = h > 0 ? h + (h == 1 ? ' hour, ' : ' hours, ') : '';
   // var mDisplay = m > 0 ? m + (m == 1 ? ' minute, ' : ' minutes, ') : '';
   // var sDisplay = s > 0 ? s + (s == 1 ? ' second' : ' seconds') : '';
-  var hDisplay = h > 0 ? h + (h == 1 ? ' hour, ' : ' hours, ') : '';
-  var mDisplay = m > 0 ? m : '0';
+  // var hDisplay = h > 0 ? h + (h == 1 ? ' hour, ' : ' hours, ') : '';
+  // var hDisplay = h > 0 ? (h < 10 ? h + ':' : '' + h) : '';
+  var hDisplay = h > 0 ? (h < 10 ? h + ':' : '' + h + ':') : '';
+  // var mDisplay = m > 0 ? m : '0';
+  var mDisplay =
+    h > 0
+      ? m > 0
+        ? m < 10
+          ? '0' + m
+          : m
+        : ''
+      : m > 0
+      ? m < 10
+        ? m
+        : m
+      : '';
   var sDisplay = s > 0 ? (s < 10 ? ':0' + s : ':' + s) : ':00';
   lengthFormatted = hDisplay + mDisplay + sDisplay;
   // console.log(h, m, s);
