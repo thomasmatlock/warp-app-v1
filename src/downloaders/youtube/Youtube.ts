a19118c654590f24885a2dc021ee8062dc6757d7
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-08-23 19:14:58 -0400

    OO

--- src/downloaders/youtube/Youtube.ts
+++ src/downloaders/youtube/Youtube.ts
@@ -1,41 +1,33 @@
 import ytdl from 'ytdl-core';
 import moment from 'moment';
 import fs from 'fs';
-let url: string = 'http://www.youtube.com/watch?v=aqz-KE-bpKQ';
-import {
-  app,
-  BrowserView,
-  BrowserWindow,
-  desktopCapturer,
-  dialog,
-  globalShortcut,
-  nativeTheme,
-  shell,
-  ipcMain,
-  screen,
-  Tray,
-} from 'electron';
+// let url: string = 'http://www.youtube.com/watch?v=aqz-KE-bpKQ';
+// import {
+//   app,
+//   BrowserView,
+//   BrowserWindow,
+//   desktopCapturer,
+//   dialog,
+//   globalShortcut,
+//   nativeTheme,
+//   shell,
+//   ipcMain,
+//   screen,
+//   Tray,
+// } from 'electron';
 import { getInfo } from './getInfo';
 import getFileSize from './getFileSize';
 import formatLength from './formatLength';
 import formatTitle from './formatTitle';
 import getQuality from './getQuality';
 export default async function Youtube(itemURL, prefs, mode) {
-  // console.log(prefs.video);
-  // console.log(prefs.audio.dropdowns[0].defaultValue);
-  // console.log(prefs.audio.dropdowns[1].defaultValue);
   let audioFormat = prefs.audio.dropdowns[1].defaultValue.label;
   let videoFormat = prefs.video.dropdowns[1].defaultValue.label;
   let audioQuality = prefs.audio.dropdowns[0].defaultValue.label;
   let videoQuality = prefs.video.dropdowns[0].defaultValue.label;
-  // console.log(audioQuality, videoQuality);
-
-  // console.log(videoFormat);
-
   let itemDetails = {};
   try {
     await ytdl.getBasicInfo(itemURL).then((info) => {
-      // console.log(info.videoDetails);
       let matchedFormat;
       if (mode.includes('audio')) {
         matchedFormat = getQuality(mode, audioQuality, info.formats);
@@ -43,16 +35,7 @@ export default async function Youtube(itemURL, prefs, mode) {
       if (mode.includes('video')) {
         matchedFormat = getQuality(mode, videoQuality, info.formats);
       }
-      // console.log(info.formats);
-      // console.log('YOUTUBE.TS', matchedFormat);
-
       itemDetails = info.videoDetails;
-      // itemDetails.background =
-      //   info.videoDetails.thumbnail.thumbnails[
-      //     info.videoDetails.thumbnail.thumbnails.length - 1
-      //   ];
-      // console.log(matchedFormat.fps);
-
       itemDetails.titleFS = formatTitle(itemDetails.title);
       itemDetails.fps = mode === 'video' ? matchedFormat.fps : '';
       itemDetails.resolution =
@@ -69,39 +52,47 @@ export default async function Youtube(itemURL, prefs, mode) {
       itemDetails.thumbnailDisplay = itemDetails.thumbnails[1].url;
       itemDetails.searchTags =
         itemDetails.ownerChannelName + ' ' + itemDetails.titleFS;
-      //////////////////////////////
-      // try {
-      //   // const video =
+      const video = ytdl(itemURL, {
+        // requestOptions: {
+        //     // headers: {
+        //     //     cookie: COOKIE,
+        //     //     // Optional. If not given, ytdl-core will try to find it.
+        //     //     // You can find this by going to a video's watch page, viewing the source,
+        //     //     // and searching for "ID_TOKEN".
+        //     //     // 'x-youtube-identity-token': 1324,
+        //     // },
+        // },
+      });
 
-      //   ytdl('http://www.youtube.com/watch?v=aqz-KE-bpKQ').pipe(
-      //     fs.createWriteStream('video.mp4')
-      //   );
-      // } catch (error) {}
-      // console.log(video);
-
-      //////////////////////////////
-      // video.on('progress', (chunkLength, downloaded, total) => {
-      //   const percent = downloaded / total;
-      //   console.log(downloaded, total);
-      //   //  lastDownloaded = downloaded;
-      //   //  if (!inserted) {
-      //   //    items.insertPercentDownloaded(this.itemInfo, percent, 'add');
-
-      //   //    inserted = true;
-      //   //  }
-      //   //  if (inserted) {
-      //   //  }
-      //   // if (lastDownloaded = downloaded) {
-      //   //  setTimeout(() => {
-      //   //    if ((downloaded = total)) {
-      //   //      items.insertPercentDownloaded(
-      //   //        this.itemInfo,
-      //   //        percent,
-      //   //        'complete'
-      //   //      );
-      //   //    }
-      //   //  }, 2000);
+      // video.on('info', info => {
+      //     console.log('title:', info.videoDetails.title);
+      //     console.log('rating:', info.player_response.videoDetails.averageRating);
+      //     console.log('uploaded by:', info.videoDetails.author.name);
       // });
+
+      // let inserted = false;
+      // let completed = false;
+      // let lastDownloaded = 0;
+      video.on('progress', (chunkLength, downloaded, total) => {
+        let percent = downloaded / total;
+        percent = Math.round(percent * 100);
+        console.log(percent);
+        // console.log(downloaded, total);
+        // lastDownloaded = downloaded;
+        // if (!inserted) {
+        //   items.insertPercentDownloaded(this.itemInfo, percent, 'add');
+
+        //   inserted = true;
+        // }
+        // if (inserted) {
+        // }
+        // if (lastDownloaded = downloaded) {
+        // setTimeout(() => {
+        //   if ((downloaded = total)) {
+        //     items.insertPercentDownloaded(this.itemInfo, percent, 'complete');
+        //   }
+        // }, 2000);
+      });
     });
   } catch (error) {
     console.log(itemURL, error);
@@ -109,6 +100,3 @@ export default async function Youtube(itemURL, prefs, mode) {
 
   return itemDetails;
 }
-// module.exports = {
-//   download,
-// };
