44116a7a25d693412b2f48f8f91bb7c21638d7e2
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-08-25 16:14:02 -0400

    Update menu.ts

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -54,7 +54,7 @@ export default class MenuBuilder {
 
   buildDarwinTemplate(): MenuItemConstructorOptions[] {
     const subMenuAbout: DarwinMenuItemConstructorOptions = {
-      label: 'Warp',
+      label: '&Warp',
       submenu: [
         // {
         //   label: 'About ElectronReact',
