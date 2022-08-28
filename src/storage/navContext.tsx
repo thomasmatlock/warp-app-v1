39c96ad9305d3dd7551dcfea941f726eee57dc17
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-08-27 20:01:58 -0400

    Update navContext.tsx

--- src/storage/navContext.tsx
+++ src/storage/navContext.tsx
@@ -61,28 +61,26 @@ export const NavContextProvider = (props) => {
   const audioPlaceholders = [
     'audio search...beep boop',
     'start typing to instantly find any audio file',
-    'hit Escape to clear your search and start over',
     'Dual Search: start typing then press Enter to also search your built-in browser',
     'Tip: Press Shift+2 to instantly switch to Video Mode',
     'Tip: Press Shift+3 to instantly switch to Warpstagram Mode',
+    'any time you want, hit Escape to clear your search and start over. Bada bing bada cleared.',
   ];
   const videoPlaceholders = [
     'video search...boop beep',
     'start typing to instantly find any video file',
-    'hit Escape to clear your search and start over',
     'Dual Search: start typing then press Enter to also search your built-in browser',
     'Tip: Press Shift+1 to instantly switch to Audio Mode',
     'Tip: Press Shift+3 to instantly switch to Warpstagram Mode',
+    'any time you want, hit Escape to clear your search and start over. Bada bing bada cleared.',
   ];
   const warpstagramPlaceholders = [
     'start typing any Instagram username, hashtag, or location',
     'Enter Instagram username, hashtag, or location',
-    'hit Escape to clear your search and start over',
+    'any time you want, hit Escape to clear your search and start over. Bada bing bada cleared.',
     'Tip: Press Shift+1 to instantly switch to Audio Mode',
     'Tip: Press Shift+2 to instantly switch to Video Mode',
-    // 'Enter instagram username',
   ];
-  // const audioSearchPlaceholder = 'audio search...beep boop';
   const audioSearchPlaceholder = randomFromArray(audioPlaceholders);
   const videoSearchPlaceholder = randomFromArray(videoPlaceholders);
   const warpstagramSearchPlaceholder = randomFromArray(warpstagramPlaceholders);
