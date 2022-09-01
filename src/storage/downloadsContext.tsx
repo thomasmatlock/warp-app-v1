112df6c7759ab5e5136f8474f867d217a90d9c21
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-08-31 23:16:09 -0400

    Revert "Revert "good reset point""
    This reverts commit f3820524dab8a86cd8777923bedd55d7d90b4684.

--- src/storage/downloadsContext.tsx
+++ src/storage/downloadsContext.tsx
@@ -47,7 +47,6 @@ const DownloadsContext = React.createContext({
 const getDownloadID = (id) => {
   // console.log(id);
 };
-
 let audioDownloadsPushed = false;
 let videoDownloadsPushed = false;
 let lastDownloadID = 0;
@@ -139,7 +138,6 @@ export const DownloadsContextProvider = (props) => {
       );
     }
   };
-
   const showInFolder = (downloadID: string) => {
     // console.log('show in folder', downloadID);
     let matchingDownload = getMatchingDownload(downloadID);
