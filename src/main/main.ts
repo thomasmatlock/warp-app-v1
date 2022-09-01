6998466bd9cf4490a8a7b9da291ddc86cffdfb5b
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-08-31 22:57:50 -0400

    Revert "Update main.ts"
    This reverts commit 153f79b627686d73d98ad1a4fd2267e6737cf8ed.

--- src/main/main.ts
+++ src/main/main.ts
@@ -74,8 +74,6 @@ import testUrls from '../downloaders/youtube/testURLS';
 // import { v4 as uuidv4 } from 'uuid';
 import createCustomer from '../payments/stripe';
 let randomYoutubeURL =
-  testUrls.youtube[Math.floor(Math.random() * testUrls.youtube.length)];
-let randomYoutubePlaylistURL =
   testUrls.youtubePlaylists[
     Math.floor(Math.random() * testUrls.youtubePlaylists.length)
   ];
