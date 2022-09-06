347e128838cefdb71c9f39ee29c571dae14d789d
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-09-06 15:46:46 -0400

    awesome

--- src/main/User.ts
+++ src/main/User.ts
@@ -4,191 +4,48 @@ const Cryptr = require('cryptr');
 const cryptr = new Cryptr('user');
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
+import UserOffline from './UserOffline';
 import { machineId, machineIdSync } from 'node-machine-id';
 let machine_id;
 async function getMachineId() {
   machine_id = await machineId();
   return machine_id;
 }
-let userDefaults = {
-  id: '',
-  isEULAaccepted: false,
-  audioDownloadsCount: 0,
-  videoDownloadsCount: 0,
-  warpstagramDownloadsCount: 0,
-  audio: 'free',
-  video: 'free',
-  warpstagram: 'free',
-  audioAuthCode: '',
-  videoAuthCode: '',
-  warpstagramAuthCode: '',
-};
+export async function createUser() {
+  console.log('creating user');
 
-function encryptUser(obj) {
-  let newObj = { ...obj };
-  for (const key in obj) {
-    newObj[key] = cryptr.encrypt(obj[key]);
-  }
-  return newObj;
-}
-function decryptUser(obj) {
-  let newObj = { ...obj };
-  for (const key in obj) {
-    newObj[key] = cryptr.decrypt(obj[key]);
-  }
-  return newObj;
-}
-function setUser(user: any) {
-  let encryptedUser = encryptUser(user);
-  settings.set('user', encryptedUser);
-}
-function resetUser() {
-  settings.delete('user');
-}
-function increaseAudioDownloadCount() {
-  // let decryptedUser = getUser();
-  // let updatedUser = { ...decryptedUser };
-  // updatedUser.audioDownloadsCount++;
-  // setUser(updatedUser);
-}
-function increaseVideoDownloadCount() {
-  // let decryptedUser = getUser();
-  // let updatedUser = { ...decryptedUser };
-  // updatedUser.videoDownloadsCount++;
-  // setUser(updatedUser);
-}
-
-function increaseWarpstagramDownloadCount() {
-  // let decryptedUser = getUser();
-  // let updatedUser = { ...decryptedUser };
-  // updatedUser.warpstagramDownloadsCount++;
-  // setUser(updatedUser);
-}
-function getAudioDownloadsCount() {
-  // let decryptedUser = getUser();
-  // return decryptedUser.audioDownloadsCount;
-}
-function getVideoDownloadsCount() {
-  // let decryptedUser = getUser();
-  // return decryptedUser.videoDownloadsCount;
-}
-function getWarpstagramDownloadsCount() {
-  // let decryptedUser = getUser();
-  // return decryptedUser.warpstagramDownloadsCount;
-}
-function canUserDownloadAudio() {
-  // let decryptedUser = getUser();
-  // if (decryptedUser.audio === 'free') {
-  //   if (decryptedUser.audioDownloadsCount < 16) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // } else {
-  //   return true;
-  // }
-}
-function canUserDownloadVideo() {
-  // let decryptedUser = getUser();
-  // if (decryptedUser.video === 'free') {
-  //   if (decryptedUser.videoDownloadsCount < 16) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // } else {
-  //   return true;
-  // }
-}
-function canUserDownloadWarpstagram() {
-  // let decryptedUser = getUser();
-  // if (decryptedUser.warpstagram === 'free') {
-  //   if (decryptedUser.warpstagramDownloadsCount < 3) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // } else {
-  //   return true;
-  // }
-}
-function upgradeUserModule(moduleType: string, moduleEdition: string) {
-  // // console.log(moduleType, moduleEdition);
-  // let decryptedUser = getUser();
-  // let updatedUser = { ...decryptedUser };
-  // for (const key in updatedUser) {
-  //   if (moduleType.includes(key)) {
-  //     updatedUser[key] = moduleEdition;
-  //   }
-  // }
-  // setUser(updatedUser);
-  // // console.log(updatedUser);
-  // return updatedUser;
-}
-import { faker } from '@faker-js/faker';
-export async function createUser() {}
-const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
-export async function getUser() {
-  console.log('getting user');
-
-  let userFromDB;
-  try {
-    userFromDB = await prisma.user.findMany({
-      where: {
-        machine_1_id: await getMachineId(),
-      },
-    });
-    console.log('found user!');
-    console.log(userFromDB);
-
-    if (userFromDB) {
-      return userFromDB;
-    } else if (userFromDB === null) {
-      // createUser();
-    }
-
-    // console.log('userFromDB', userFromDB);
-    // return userFromDB;
-  } catch (error) {}
-  //   userFromDB = createUser();
-  //   return userFromDB;
-  // }
   const user = await prisma.user.create({
     data: {
       machine_1_id: await getMachineId(),
     },
   });
-  // console.log(user);
+  console.log('user created');
+  console.log(user);
   return user;
-  // resetUser(); // REMOVE, FOR TESTING ONLY
-  // let encryptedUser = settings.get('user');
-  // if (encryptedUser === undefined) {
-  //   setUser(userDefaults);
-  //   return userDefaults;
-  // } else {
-  //   encryptedUser = settings.get('user');
-  //   // console.log(encryptedUser);
-  //   let decryptedUser = decryptUser(encryptedUser);
-  //   return decryptedUser;
-  // }
+}
+export async function getUser() {
+  console.log('getting user');
+
+  let userFromDB;
+  try {
+    userFromDB = await prisma.user.findFirst({
+      where: {
+        machine_1_id: await getMachineId(),
+      },
+    });
+    if (userFromDB === null) {
+      console.log('user not found, creating user');
+      userFromDB = await createUser();
+      return userFromDB;
+    } else if (userFromDB) {
+      console.log('found user!');
+      return userFromDB;
+    }
+  } catch (error) {}
+  userFromDB = await createUser();
+  return userFromDB;
 }
 
 module.exports = {
   getUser: getUser,
-  setUser: setUser,
-  increaseAudioDownloadCount: increaseAudioDownloadCount,
-  increaseVideoDownloadCount: increaseVideoDownloadCount,
-  increaseWarpstagramDownloadCount: increaseWarpstagramDownloadCount,
-  getAudioDownloadsCount: getAudioDownloadsCount,
-  getVideoDownloadsCount: getVideoDownloadsCount,
-  getWarpstagramDownloadsCount: getWarpstagramDownloadsCount,
-  canUserDownloadAudio: canUserDownloadAudio,
-  canUserDownloadVideo: canUserDownloadVideo,
-  canUserDownloadWarpstagram: canUserDownloadWarpstagram,
-  upgradeUserModule: upgradeUserModule,
-  resetUser: resetUser,
 };
-// USE TO UPGRADE USER
-// User.upgradeUserModule('audio', 'free');
-// User.upgradeUserModule('video', 'free');
-// User.upgradeUserModule('warpstagram', 'free');
---
+++ src/main/UserOffline.ts
@@ -0,0 +1,164 @@
+const Store = require('electron-store');
+const settings = new Store();
+const Cryptr = require('cryptr');
+const cryptr = new Cryptr('user');
+import { machineId, machineIdSync } from 'node-machine-id';
+let machine_id;
+async function getMachineId() {
+  machine_id = await machineId();
+  return machine_id;
+}
+let userDefaults = {
+  id: '',
+  email: '',
+  createdAt: Date.now(),
+  updatedAt: Date.now(),
+  machine_1_id: '',
+  machine_2_id: '',
+  machine_3_id: '',
+  isEULAaccepted: false,
+  audioDownloadsCount: 0,
+  videoDownloadsCount: 0,
+  warpstagramDownloadsCount: 0,
+  audio: 'free',
+  video: 'free',
+  warpstagram: 'free',
+  audioAuthCode: '',
+  videoAuthCode: '',
+  warpstagramAuthCode: '',
+};
+
+function encryptUser(obj) {
+  let newObj = { ...obj };
+  for (const key in obj) {
+    newObj[key] = cryptr.encrypt(obj[key]);
+  }
+  return newObj;
+}
+function decryptUser(obj) {
+  let newObj = { ...obj };
+  for (const key in obj) {
+    newObj[key] = cryptr.decrypt(obj[key]);
+  }
+  return newObj;
+}
+function setUser(user: any) {
+  let encryptedUser = encryptUser(user);
+  settings.set('user', encryptedUser);
+}
+function resetUser() {
+  settings.delete('user');
+}
+function increaseAudioDownloadCount() {
+  let decryptedUser = getUser();
+  let updatedUser = { ...decryptedUser };
+  updatedUser.audioDownloadsCount++;
+  setUser(updatedUser);
+}
+function increaseVideoDownloadCount() {
+  let decryptedUser = getUser();
+  let updatedUser = { ...decryptedUser };
+  updatedUser.videoDownloadsCount++;
+  setUser(updatedUser);
+}
+
+function increaseWarpstagramDownloadCount() {
+  let decryptedUser = getUser();
+  let updatedUser = { ...decryptedUser };
+  updatedUser.warpstagramDownloadsCount++;
+  setUser(updatedUser);
+}
+function getAudioDownloadsCount() {
+  let decryptedUser = getUser();
+  return decryptedUser.audioDownloadsCount;
+}
+function getVideoDownloadsCount() {
+  let decryptedUser = getUser();
+  return decryptedUser.videoDownloadsCount;
+}
+function getWarpstagramDownloadsCount() {
+  let decryptedUser = getUser();
+  return decryptedUser.warpstagramDownloadsCount;
+}
+function canUserDownloadAudio() {
+  let decryptedUser = getUser();
+  if (decryptedUser.audio === 'free') {
+    if (decryptedUser.audioDownloadsCount < 16) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return true;
+  }
+}
+function canUserDownloadVideo() {
+  let decryptedUser = getUser();
+  if (decryptedUser.video === 'free') {
+    if (decryptedUser.videoDownloadsCount < 16) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return true;
+  }
+}
+function canUserDownloadWarpstagram() {
+  let decryptedUser = getUser();
+  if (decryptedUser.warpstagram === 'free') {
+    if (decryptedUser.warpstagramDownloadsCount < 3) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return true;
+  }
+}
+function upgradeUserModule(moduleType: string, moduleEdition: string) {
+  // console.log(moduleType, moduleEdition);
+  let decryptedUser = getUser();
+  let updatedUser = { ...decryptedUser };
+  for (const key in updatedUser) {
+    if (moduleType.includes(key)) {
+      updatedUser[key] = moduleEdition;
+    }
+  }
+  setUser(updatedUser);
+  // console.log(updatedUser);
+  return updatedUser;
+}
+export async function getUser() {
+  // resetUser(); // REMOVE, FOR TESTING ONLY
+  let encryptedUser = settings.get('user');
+  if (encryptedUser === undefined) {
+    setUser(userDefaults);
+    return userDefaults;
+  } else {
+    encryptedUser = settings.get('user');
+    // console.log(encryptedUser);
+    let decryptedUser = decryptUser(encryptedUser);
+    return decryptedUser;
+  }
+}
+
+module.exports = {
+  getUser: getUser,
+  setUser: setUser,
+  increaseAudioDownloadCount: increaseAudioDownloadCount,
+  increaseVideoDownloadCount: increaseVideoDownloadCount,
+  increaseWarpstagramDownloadCount: increaseWarpstagramDownloadCount,
+  getAudioDownloadsCount: getAudioDownloadsCount,
+  getVideoDownloadsCount: getVideoDownloadsCount,
+  getWarpstagramDownloadsCount: getWarpstagramDownloadsCount,
+  canUserDownloadAudio: canUserDownloadAudio,
+  canUserDownloadVideo: canUserDownloadVideo,
+  canUserDownloadWarpstagram: canUserDownloadWarpstagram,
+  upgradeUserModule: upgradeUserModule,
+  resetUser: resetUser,
+};
+// USE TO UPGRADE USER
+// User.upgradeUserModule('audio', 'free');
+// User.upgradeUserModule('video', 'free');
+// User.upgradeUserModule('warpstagram', 'free');
