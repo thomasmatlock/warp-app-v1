8fb3462b16ba9b5e73abf0b6464ae7676ba3b24b
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-09-13 21:07:10 -0400

    update reconfig

--- src/main/updater.ts
+++ src/main/updater.ts
@@ -4,12 +4,6 @@ import log from 'electron-log';
 log.transports.file.level = 'info';
 autoUpdater.autoDownload = false;
 autoUpdater.logger = log;
-// autoUpdater.setFeedURL({
-//   provider: 'github',
-//   owner: 'thomasmatlock',
-//   repo: 'warp-app',
-//   token: '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',
-// });
 export default function () {
   console.log('checking for update');
   // mWin.webContents.send('update_available', [item, 'video']);
@@ -20,14 +14,33 @@ export default function () {
   // //   });
   autoUpdater.checkForUpdates();
   autoUpdater.on('update-available', () => {
-    dialog.showMessageBox({
-      type: 'info',
-      title: 'Update available',
-      message: 'A new version of Warp is available. Do you want to update now?',
-      buttons: ['Update', 'No'],
-    });
     console.log('update available');
+    autoUpdater.downloadUpdate();
+    dialog
+      .showMessageBox({
+        type: 'info',
+        title: 'Update available',
+        message:
+          'A new version of Warp is available. Do you want to update now?',
+        buttons: ['Update', 'No'],
+      })
+      .then((result) => {
+        let buttonIndex = result.response;
+        if (buttonIndex === 0) autoUpdater.downloadUpdate();
+      });
   });
-  // console.log('update available');
-  // console.log('updater');
+  autoUpdater.on('update-downloaded', () => {
+    console.log('update downloaded');
+    dialog
+      .showMessageBox({
+        type: 'info',
+        title: 'Install Updates',
+        message: 'Updates downloaded, install and restart now?',
+        buttons: ['Yes', 'Later'],
+      })
+      .then((result) => {
+        let buttonIndex = result.response;
+        if (buttonIndex === 0) autoUpdater.quitAndInstall(false, true); // arg1 is silent install, arg2 is force run after install
+      });
+  });
 }
