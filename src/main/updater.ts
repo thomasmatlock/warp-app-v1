c7496f8a907b7f998789acd557aa7301203d3335
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-09-13 23:05:31 -0400

    Update updater.ts

--- src/main/updater.ts
+++ src/main/updater.ts
@@ -8,36 +8,36 @@ export default function () {
   autoUpdater.checkForUpdates();
   autoUpdater.on('update-available', () => {
     autoUpdater.downloadUpdate();
-    // dialog
-    //   .showMessageBox({
-    //     type: 'info',
-    //     title: 'Update available',
-    //     message:
-    //       'A new version of Warp is available. Do you want to update now?',
-    //     buttons: ['Update', 'No'],
-    //   })
-    //   .then((result) => {
-    //     let buttonIndex = result.response;
-    //     if (buttonIndex === 0) autoUpdater.downloadUpdate();
-    //   });
+    dialog
+      .showMessageBox({
+        type: 'info',
+        title: 'Update available',
+        message:
+          'A new version of Warp is available. Do you want to update now?',
+        buttons: ['Update', 'No'],
+      })
+      .then((result) => {
+        let buttonIndex = result.response;
+        if (buttonIndex === 0) autoUpdater.downloadUpdate();
+      });
   });
   autoUpdater.on('update-downloaded', () => {
-    autoUpdater.quitAndInstall(true, true); // arg1 is silent install, arg2 is force run after install
-    app.quit();
-    // console.log('update downloaded');
-    // dialog
-    //   .showMessageBox({
-    //     type: 'info',
-    //     title: 'Install Updates',
-    //     message: 'Updates downloaded, install and restart now?',
-    //     buttons: ['Yes', 'Later'],
-    //   })
-    //   .then((result) => {
-    //     let buttonIndex = result.response;
-    //     if (buttonIndex === 0) {
-    //       autoUpdater.quitAndInstall(true, true); // arg1 is silent install, arg2 is force run after install
-    //       app.quit();
-    //     }
-    //   });
+    // autoUpdater.quitAndInstall(true, true); // arg1 is silent install, arg2 is force run after install
+    // app.quit();
+    console.log('update downloaded');
+    dialog
+      .showMessageBox({
+        type: 'info',
+        title: 'Install Updates',
+        message: 'Updates downloaded, install and restart now?',
+        buttons: ['Yes', 'Later'],
+      })
+      .then((result) => {
+        let buttonIndex = result.response;
+        if (buttonIndex === 0) {
+          autoUpdater.quitAndInstall(true, true); // arg1 is silent install, arg2 is force run after install
+          app.quit();
+        }
+      });
   });
 }
