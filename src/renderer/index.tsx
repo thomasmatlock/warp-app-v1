ec148c432e211ee6f5d602dc4028bb285e40c026
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-09-21 15:54:49 -0400

    doing last part of move

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,13 +1,38 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { ThemeContextProvider } from '../storage/themeContext';
+import { ActionBarContextProvider } from '../storage/actionBarContext';
+import { SourcesContextProvider } from '../storage/sourcesContext';
+import { DownloadsContextProvider } from '../storage/downloadsContext';
+import { NavContextProvider } from '../storage/navContext';
+import { InputContextProvider } from '../storage/inputContext';
+import { ModalsContextProvider } from '../storage/modalsContext';
+import { PrefsContextProvider } from '../storage/prefsContext';
+import { ProductsContextProvider } from '../storage/productsContext';
+import { UserContextProvider } from '../storage/userContext';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
-root.render(<App />);
-
-// calling IPC exposed from preload script
-window.electron.ipcRenderer.once('ipc-example', (arg) => {
-  // eslint-disable-next-line no-console
-  console.log(arg);
-});
-window.electron.ipcRenderer.sendMessage('ipc-example', ['ping']);
+root.render(
+  <ThemeContextProvider>
+    <DownloadsContextProvider>
+      <UserContextProvider>
+        <PrefsContextProvider>
+          <NavContextProvider>
+            <ProductsContextProvider>
+              <ModalsContextProvider>
+                <InputContextProvider>
+                  <SourcesContextProvider>
+                    <ActionBarContextProvider>
+                      <App />
+                    </ActionBarContextProvider>
+                  </SourcesContextProvider>
+                </InputContextProvider>
+              </ModalsContextProvider>
+            </ProductsContextProvider>
+          </NavContextProvider>
+        </PrefsContextProvider>
+      </UserContextProvider>
+    </DownloadsContextProvider>
+  </ThemeContextProvider>
+);
