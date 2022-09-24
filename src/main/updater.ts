dbb4fa342702ee1c4770bb96172f4f225ebc5436
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-09-24 01:52:20 -0400

    awesome

--- src/main/updater.ts
+++ src/main/updater.ts
@@ -4,31 +4,22 @@
 import log from 'electron-log';
 import { app, BrowserWindow, ipcMain } from 'electron';
 import { autoUpdater } from 'electron-updater';
-import path from 'path';
-import fs from 'fs';
 
 log.transports.file.level = 'info';
 autoUpdater.autoDownload = false;
 autoUpdater.logger = log;
-// autoUpdater.updateConfigPath = path.join(app.getPath('temp'), 'warp-updater');
-// async function isEmptyDir(path) {
-//   try {
-//     const directory = await fs.opendir(path);
-//     const entry = await directory.read();
-//     await directory.close();
 
-//     return entry === null;
-//   } catch (error) {
-//     return false;
-//   }
-// }
-
-let updateDownloaded = false;
 export default function (mWin: BrowserWindow) {
   // mWin.webContents.send('update-not-available', '');
-  mWin.webContents.send('checking-for-update', 'checking for update...');
   if (!app.isPackaged) {
     mWin.webContents.send('update-not-available', '');
+  }
+  if (app.isPackaged) {
+    mWin.webContents.send('checking-for-update', 'checking for update...');
+    autoUpdater.on('update-available', () => {
+      mWin.webContents.send('update-available', 'downloading update...');
+      //  autoUpdater.downloadUpdate();
+    });
   }
   setTimeout(() => {
     if (app.isPackaged) {
@@ -49,64 +40,10 @@ export default function (mWin: BrowserWindow) {
         );
       });
       autoUpdater.on('update-downloaded', () => {
-        // if (process.platform === 'win32') {
         mWin.webContents.send(
-          'checking-for-update',
-          'checking update integrity...'
+          'update-downloaded',
+          ' An updated version of Warp is ready to be installed at the next app launch.'
         );
-        // }
-        // setTimeout(() => {
-        if (process.platform === 'win32') {
-          let binaryDirectory = path.join(
-            app.getPath('home'),
-            'AppData',
-            'Local',
-            'warp-updater',
-            'pending'
-          );
-          fs.readdir(binaryDirectory, function (err, files) {
-            if (err) {
-              // throw err;
-              // some sort of error
-            } else if (!files.length) {
-              // directory appears to be empty
-              mWin.webContents.send(
-                'update-available',
-                'Update not saved, trying again...'
-              );
-              autoUpdater.downloadUpdate();
-            } else {
-              // directory contains files
-              // updateDownloaded = true;
-              // mWin.webContents.send('update-downloaded', 'Update downloaded');
-              mWin.webContents.send(
-                'update-downloaded',
-                ' An updated version of Warp is ready to be installed at the next app launch.'
-              );
-              // mWin.webContents.send('update-downloaded', 'Update downloaded');
-            }
-          });
-          // isEmptyDir(binaryDirectory).then((empty) => {
-          // if (empty) {
-
-          // } else {
-
-          // mWin.webContents.send(
-          //   'update-downloaded',
-          //   'Update not saved, trying again...'
-          // );
-          // mWin.webContents.send('update-not-available', '');
-
-          // });
-        } else if (process.platform === 'darwin') {
-          // mWin.webContents.send('update-downloaded', 'Update downloaded');
-          // updateDownloaded = true;
-          mWin.webContents.send(
-            'update-downloaded',
-            ' An updated version of Warp is ready to be installed at the next app launch.'
-          );
-        }
-        // }, 3000);
       });
       ipcMain.on('restart_and_update', () => {
         autoUpdater.quitAndInstall(true, true); // arg1 is silent install, arg2 is force run after install
