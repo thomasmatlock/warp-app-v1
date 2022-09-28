2733445fe9f6e5fc03814f4b94429257a9c199d9
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-09-27 23:21:52 -0400

    try this

--- src/main/UserOffline.ts
+++ src/main/UserOffline.ts
@@ -5,11 +5,14 @@ const settings = new Store();
 const Cryptr = require('cryptr');
 const cryptr = new Cryptr('user');
 // process.env.INIT_CWD = process.cwd();
-import { PrismaClient } from '@prisma/client'; // trying to use the prisma client here with new path
+// import { PrismaClient } from 'C:\\Users\\Tommy\\Documents\\GitHub\\warp-app\\node_modules\\.prisma\\client\\'; // trying to use the prisma client here with new path
+import { PrismaClient } from 'C:\\Program Files\\Warp\\resources\\node_modules\\@prisma\\client'; // trying to use the prisma client here with new path
+// import { PrismaClient } from '../@prisma/client'; // trying to use the prisma client here with new path
 //        datamodelPath: 'C:\\Users\\Tommy\\Documents\\GitHub\\warp-app\\node_modules\\.prisma\\client\\schema.prisma',
 // let schemaLocation = path.join(process.cwd(), 'prisma', 'schema.prisma');
 // const prisma = new PrismaClient({ schema: fs.readFileSync(schemaLocation) });
 // const prisma = new PrismaClient({ _engineConfig: schemaLocation });
+
 const prisma = new PrismaClient({
   //   datamodelPath: '/node_modules/.prisma/client/schema.prisma',
 });
