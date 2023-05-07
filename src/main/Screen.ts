67e5740a51d69518d6f162c601cf925e54f0e28c
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2023-05-07 19:20:41 -0400

    k

--- src/main/Screen.ts
+++ src/main/Screen.ts
@@ -4,47 +4,47 @@ const Store = require('electron-store');
 
 const settings = new Store();
 export default class Screen {
-  mWin: BrowserWindow;
+	mWin: BrowserWindow;
 
-  defaults: unknown;
+	defaults: unknown;
 
-  screenState: unknown;
+	screenState: unknown;
 
-  newState: unknown;
+	newState: unknown;
 
-  constructor(mWin: BrowserWindow) {
-    this.mWin = mWin;
-    this.defaults = {
-      bounds: {
-        x: 0,
-        y: 0,
-        width: 1600,
-        height: 900,
-      },
-      isMaximized: true,
-    };
-  }
+	constructor(mWin: BrowserWindow) {
+		this.mWin = mWin;
+		this.defaults = {
+			bounds: {
+				x: 0,
+				y: 0,
+				width: 1600,
+				height: 900,
+			},
+			isMaximized: true,
+		};
+	}
 
-  getScreenState = () => {
-    this.screenState = settings.get('screen');
-    if (this.screenState === undefined) {
-      settings.set('screen', this.defaults);
-      return this.defaults as object;
-    }
-    return this.screenState as object;
-  };
+	getScreenState = () => {
+		this.screenState = settings.get('screen');
+		if (this.screenState === undefined) {
+			settings.set('screen', this.defaults);
+			return this.defaults as object;
+		}
+		return this.screenState as object;
+	};
 
-  setScreenState = (mWin: BrowserWindow) => {
-    const newState = this.getScreenState() as any;
-    const newBounds = mWin.getBounds();
-    newState.bounds = newBounds;
-    newState.isMaximized = mWin.isMaximized();
-    settings.set('screen', newState);
-    this.screenState = newState;
-  };
+	setScreenState = (mWin: BrowserWindow) => {
+		const newState = this.getScreenState() as any;
+		const newBounds = mWin.getBounds();
+		newState.bounds = newBounds;
+		newState.isMaximized = mWin.isMaximized();
+		settings.set('screen', newState);
+		this.screenState = newState;
+	};
 
-  resetScreenState = () => {
-    settings.delete('screenState');
-    return this.defaults;
-  };
+	resetScreenState = () => {
+		settings.delete('screenState');
+		return this.defaults;
+	};
 }
--- src/renderer/Products/Product.tsx
+++ src/renderer/Products/Product.tsx
@@ -11,62 +11,60 @@ import styles from './Products.module.scss';
 import Scene from '../Models/Scene';
 
 export default function Product(props: any) {
-  // window.electron.ipcRenderer.on('global', (arg) => {
-  //   console.log(arg);
-  // });
-  // console.log(props);
+	// window.electron.ipcRenderer.on('global', (arg) => {
+	//   console.log(arg);
+	// });
+	// console.log(props);
 
-  const {
-    id,
-    image,
-    title,
-    description,
-    features,
-    ctaMessage,
-    expanded,
-    collapsed,
-    threeScene,
-  } = props;
-  // console.log(expanded);
+	const {
+		id,
+		image,
+		title,
+		description,
+		features,
+		ctaMessage,
+		expanded,
+		collapsed,
+		threeScene,
+	} = props;
+	// console.log(expanded);
 
-  const productsCtx = useContext(ProductsContext);
-  // console.log(productsCtx);
+	const productsCtx = useContext(ProductsContext);
+	// console.log(productsCtx);
 
-  const mouseEnterHandler = (e: any) => {
-    // productsCtx.toggleUserInteracting();
-    // productsCtx.getCardID(id);
-  };
-  const mouseLeaveHandler = (e: any) => {
-    // productsCtx.toggleUserInteracting();
-  };
-  // const activeCardClass = [styles.card__active, styles.card].join(' ');
-  const clickHandler = (e: any) => {
-    productsCtx.getCardID(id);
-  };
-  const regularCardClass = [styles.card].join(' ');
-  const expandedCardClass = [styles.card__active, styles.card].join(' ');
-  const collapsedCardClass = [styles.card__collapsed, styles.card].join(' ');
-  const cardClass = collapsed ? collapsedCardClass : regularCardClass;
-  // cardClass = collapsed ? collapsedCardClass : regularCardClass;
+	const mouseEnterHandler = (e: any) => {
+		// productsCtx.toggleUserInteracting();
+		// productsCtx.getCardID(id);
+	};
+	const mouseLeaveHandler = (e: any) => {
+		// productsCtx.toggleUserInteracting();
+	};
+	// const activeCardClass = [styles.card__active, styles.card].join(' ');
+	const clickHandler = (e: any) => {
+		productsCtx.getCardID(id);
+	};
+	const regularCardClass = [styles.card].join(' ');
+	const expandedCardClass = [styles.card__active, styles.card].join(' ');
+	const collapsedCardClass = [styles.card__collapsed, styles.card].join(' ');
+	const cardClass = collapsed ? collapsedCardClass : regularCardClass;
+	// cardClass = collapsed ? collapsedCardClass : regularCardClass;
 
-  return (
-    //                         {overviewDisplayCtx.audioMode && <a className={browserBarBtnClass1}>Download Audio MP3</a>}
-
-    <div className={cardClass} id={id}>
-      <div className={styles.card_content}>
-        <div className={styles.card_info_wrapper}>
-          <div className={styles.card_info}>
-            {/* <img className={styles.card_image} src={image} alt="" /> */}
-            <Scene threeScene={threeScene} />
-            <div className={styles.card_info_title}>
-              <h3>{title}</h3>
-              <h4>{description}</h4>
-            </div>
-            {/* <ProductFeaturesList features={features} /> */}
-            <CTAProduct message={ctaMessage} clickHandler={clickHandler} />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={cardClass} id={id}>
+			<div className={styles.card_content}>
+				<div className={styles.card_info_wrapper}>
+					<div className={styles.card_info}>
+						{/* <img className={styles.card_image} src={image} alt="" /> */}
+						<Scene threeScene={threeScene} />
+						<div className={styles.card_info_title}>
+							<h3>{title}</h3>
+							<h4>{description}</h4>
+						</div>
+						<ProductFeaturesList features={features} />
+						<CTAProduct message={ctaMessage} clickHandler={clickHandler} />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
