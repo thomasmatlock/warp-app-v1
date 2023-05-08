80f68ca522026a68223442ad89fca71b628b0c19
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2023-05-08 15:39:17 -0400

    nice

--- src/renderer/Products/ProductSubtitle.tsx
+++ src/renderer/Products/ProductSubtitle.tsx
@@ -3,8 +3,6 @@ import styles from './ProductSubtitle.module.scss';
 export default function ProductSubtitle(props: any) {
   const { description, expanded } = props;
 
-  const componentStyle = expanded
-    ? styles.card_info_title__subtitle
-    : styles.card_info_title__subtitle;
+  const componentStyle = expanded ? styles.subtitle : styles.subtitle;
   return <h3 className={componentStyle}>{description}</h3>;
 }
--- src/renderer/Products/ProductTitle.tsx
+++ src/renderer/Products/ProductTitle.tsx
@@ -3,8 +3,6 @@ import styles from './ProductTitle.module.scss';
 export default function ProductTitle(props: any) {
   const { title, expanded } = props;
 
-  const componentStyle = expanded
-    ? styles.card_info_title__title__expanded
-    : styles.card_info_title__title;
+  const componentStyle = expanded ? styles.title_expanded : styles.title;
   return <h3 className={componentStyle}>{title}</h3>;
 }
