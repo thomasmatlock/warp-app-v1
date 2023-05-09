a12c48378f367799a6e5bdc62415d26454615872
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2023-05-09 12:06:49 -0400

    k

--- src/renderer/Products/Checkout/ProductCheckout.tsx
+++ src/renderer/Products/Checkout/ProductCheckout.tsx
@@ -3,6 +3,6 @@ import styles from './ProductCheckout.module.scss';
 export default function ProductCheckout(props: any) {
   const { title, expanded } = props;
 
-  const componentStyle = expanded ? styles.checkout : styles.checkout;
+  const componentStyle = expanded ? styles.checkout__expanded : styles.checkout;
   return <>{expanded && <div className={componentStyle}>checkout NOW</div>}</>;
 }
