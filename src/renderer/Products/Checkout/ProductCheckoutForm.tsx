64b74c3468bbe7175378e1b9b2e28def93e42a1e
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2023-05-11 16:27:19 -0400

    Update ProductCheckoutForm.tsx

--- src/renderer/Products/Checkout/ProductCheckoutForm.tsx
+++ src/renderer/Products/Checkout/ProductCheckoutForm.tsx
@@ -1,18 +1,7 @@
-import { useFormik } from 'formik';
-import { useState } from 'react';
 import ProductCheckoutInputField from './ProductCheckoutInputField';
 import ProductCheckoutCreditCardForm from './ProductCheckoutCreditCardForm';
 import styles from './ProductCheckoutForm.module.scss';
-// import * strings from './ProductCheckoutInputField.strings.json';
-import {
-  validateEmail,
-  validateCreditCard,
-  validateCreditCardType,
-  validateExpirationDate,
-  validateNameOnCard,
-  validateZipCode,
-  validateCVV,
-} from '../../../main/util/strings';
+
 import { generateRandomName } from '../../../main/util/names';
 
 const fieldData = {
@@ -24,7 +13,6 @@ const fieldData = {
   nameOnCard: {
     type: 'nameOnCard',
     label: 'Name on Card',
-    // placeholder: 'John Doe',
     placeholder: generateRandomName(),
   },
   zipCode: {
@@ -34,56 +22,7 @@ const fieldData = {
   },
 };
 export default function ProductCheckoutForm(props: any) {
-  // PROPS DESTRUCTURING
   const { expanded } = props;
-  //
-  const [isInputValid, setIsInputValid] = useState(true);
-  // TYPES //////////////////////////////
-  // const [isEmailType, setIsEmailType] = useState(type === 'email');
-  // const [isCreditCardType, setIsCreditCardType] = useState(
-  //   type === 'creditCard'
-  // );
-  // const [isExpirationDateType, setIsExpirationDateType] = useState(
-  //   type === 'expirationDate'
-  // );
-  // const [isCvvType, setIsCvvType] = useState(type === 'cvv');
-  // const [isNameOnCardType, setIsNameOnCardType] = useState(
-  //   type === 'nameOnCard'
-  // );
-  // const [isZipCodeType, setIsZipCodeType] = useState(type === 'zipCode');
-  // VALID TYPES
-  // const [isInputValidEmail, setisInputValidEmail] = useState(false);
-  // const [isInputValidCreditCard, setisInputValidCreditCard] = useState(false);
-  // const [isInputValidExpirationDate, setisInputValidExpirationDate] =
-  //   useState(false);
-  // const [isInputValidCVV, setisInputValidCVV] = useState(false);
-  // const [isInputValidNameOnCard, setisInputValidNameOnCard] = useState(false);
-  // const [isInputValidZipCode, setisInputValidZipCode] = useState(false);
-  // const [isVisaCard, setIsVisaCard] = useState(false);
-  // const [isMasterCard, setIsMasterCard] = useState(false);
-  // const [isAmexCard, setIsAmexCard] = useState(false);
-  // const [isDiscoverCard, setIsDiscoverCard] = useState(false);
-  const [inputClass, setInputClass] = useState(styles.field_input);
-  // let inputClass = isInputValid ? styles.field_input : styles.field_input;
-  const changeHandler = (e: any) => {
-    if (e.target.value.length > 0) {
-      // if (isEmailType) setIsInputValid(validateEmail(e.target.value));
-      // if (isCreditCardType) setIsInputValid(validateCreditCard(e.target.value));
-      // if (isExpirationDateType)
-      //   setIsInputValid(validateExpirationDate(e.target.value));
-      // if (isCvvType) setIsInputValid(validateCVV(e.target.value));
-      // if (isNameOnCardType) setIsInputValid(validateNameOnCard(e.target.value));
-      // if (isZipCodeType) setIsInputValid(validateZipCode(e.target.value));
-    }
-  };
-
-  // };
-  const mouseEnterHandler = () => {
-    // console.log('mouse enter');
-  };
-  const mouseLeaveHandler = () => {
-    // console.log('mouse leave');
-  };
 
   const componentStyle = expanded ? styles.form : styles.form;
   return (
